1794c38af58ce54307a062e9dcff93bd3f2425c7
TassoEnzo <enzorodrigues9040@gmail.com>
2025-09-15 01:58:39 -0300

    Merge remoto com local

--- Exercicio Aula 3/Exercicio 2/main.dart
+++ Exercicio Aula 3/Exercicio 2/main.dart
@@ -7,9 +7,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
